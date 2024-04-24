fc92f92c8d1a2b802ebbb4956c6f829a1db2d198
Aleksandra Li <li0tvali@yandex.ru>
2024-04-24 07:37:58 +0300

    init

---
+++ src/main/java/ru/nesterov/clientanalyzer/ClientAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ru.nesterov.clientanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nesterov/clientanalyzer/ClientAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nesterov.clientanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
