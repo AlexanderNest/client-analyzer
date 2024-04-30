41c5a422a1498c294bc1e155aba8f59e3823990a
Aleksandra Li <li0tvali@yandex.ru>
2024-04-30 07:42:49 +0300

    исправила моделс

--- src/main/java/ru/nesterov/clientanalyzer/models/Client.java
+++ src/main/java/ru/nesterov/clientanalyzer/models/Client.java
@@ -1,25 +1,16 @@
 package ru.nesterov.clientanalyzer.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
-@Entity
-@Table (name = "client")
 public class Client {
-    @Id
-    int id;
-    String name;
-    int communication_type_id;
-    int cost_per_hour;
-    float count_of_hours_pr_week;
-    int count_of_meetings_pr_week;
-    int active;
-
+    private int id;
+    private String name;
+    private int communicationTypeId;
+    private int costPerHour;
+    private float countOfHoursPerWeek;
+    private int countOfMeetingsPerWeek;
+    private boolean active;
 }
 
--- src/main/java/ru/nesterov/clientanalyzer/models/CommunicationType.java
+++ src/main/java/ru/nesterov/clientanalyzer/models/CommunicationType.java
@@ -1,15 +1,9 @@
 package ru.nesterov.clientanalyzer.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-@Entity
-@Table(name = "communication_type")
 public class CommunicationType {
-    @Id
-    int id;
-    String name;
+    private int id;
+    private String name;
 }
